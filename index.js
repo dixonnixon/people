25d46f0bfdfa8a58f00b6ee7a5ba4a9ab197d359
dixonnixon <drenb@ukr.net>
2018-11-25 23:15:58 +0200

    m1

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const Human = require('./human');
 
-
+//find AVG in array of imported objects
+// Human (id, first_name, ...)
+// by field name
 function avg(array, field) {
     let sum = 0;
     for(let v of array) {
@@ -13,8 +15,11 @@ function avg(array, field) {
     return {value: parseFloat(sum/array.length), field: field};
 }
 
-function findFav(array, field) {
-    let fav = find(array, field);
+//find any peron from imported array
+//by most freq fiel 
+
+function findAnyFav(array, field) {
+    let fav = findMostOne(array, field);
     // console.log(fav.value);
     let findFav = [];
     for(let v of array) 
@@ -23,12 +28,14 @@ function findFav(array, field) {
     return findFav[Math.floor(Math.random()*findFav.length)];
 }
 
-function find(array, field, value) {
+//find most popular rows by field
+function findMostOne(array, field, value) {
     var frequency = {};  // array of frequency.
     var max = 0;  // holds the max frequency.
     var result;
     var resultCn;
     var cn = 0;
+    //stackOverflowCode --------------------------
     for(let v in array) {
         frequency[array[v][field]]
             = (frequency[array[v][field]] || 0) + 1; 
@@ -45,33 +52,41 @@ function find(array, field, value) {
             max = cn;
             
         }
+    //stackOverflowCode END--------------------------
     }
     return {value: (value) ? resultCn : result, count: max, field: field};
 }
 
+
+//use it after data allready imported to array 
 function searchData(array) {
-    // console.log(array);
-    // console.log(mode);
-    // console.log(array);
     
-    let name = find(array, 'first_name');
-    let country = find(array, 'country');
-    let countMen = find(array, 'gender', 'Female');
-    let countWomen = find(array, 'gender', 'Male');
+    let name = findMostOne(array, 'first_name');
+    let country = findMostOne(array, 'country');
+    let countMen = findMostOne(array, 'gender', 'Female');
+    let countWomen = findMostOne(array, 'gender', 'Male');
 
     let avgHeight = avg(array, 'height');
     let avgWeight = avg(array, 'weight');
-    let anyFavName = findFav(array, 'first_name');
-    let anyFavCountry = findFav(array, 'country');
+    let anyFavName = findAnyFav(array, 'first_name');
+    let anyFavCountry = findAnyFav(array, 'country');
 
     // console.log(result);
+    console.log(' 1)Самое популярное имя;\n ' + name.value);
     console.log(name);
-    console.log(country);
+    console.log(' 2.1)Колличество мужчин;\n' + countMen.count);
     console.log(countMen);
+    console.log(' 2.2)Колличество женщин;\n' + countWomen.count);
     console.log(countWomen);
+    console.log(' 3)Средний рост;\n' + avgHeight.value + ' sm');
     console.log(avgHeight);
+    console.log(' 4)Средний вес;\n' + avgWeight.value + ' kg');
     console.log(avgWeight);
+    console.log(' 5)Название самой популярной страны;\n' + country.value);
+    console.log(country);
+    console.log(' 6.1)Все данные о любом человеке с самым популярным именем;\n' + anyFavName.first_name);
     console.log(anyFavName);
+    console.log(' 6.2)Все данные о любом человеке из самой популярной страны;\n' + anyFavCountry.country);
     console.log(anyFavCountry);
 }
 
